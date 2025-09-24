69ebc73dffb7706ae6d832e870defdb0d655e5ee
Leonardo <info.leonardofalchi@gmail.com>
2025-09-24 19:44:37 +0200

    crezione components-inserimento img-HEADER

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,23 @@
+export default function MyHeader() {
+    return (
+        <header>
+            <nav>
+                <figure>
+                    <img src="/dc-logo.png" alt="logo" />
+                </figure>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+
+        </header>);
+}
---
+++ src/components/MyMainContent.jsx
@@ -0,0 +1,3 @@
+export default function MyMainContent() {
+    return (<main> </main>);
+}
