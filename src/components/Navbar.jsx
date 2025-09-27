c3434019438dfa65650e7edb6061860fb04e68e4
Leonardo <info.leonardofalchi@gmail.com>
2025-09-27 18:11:06 +0200

    dinamicizzazione navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
 const Navbar = () => {
+
+    const links = [
+        { id: 1, text: 'CHARACTERS', url: '#', current: false },
+        { id: 2, text: 'COMICS', url: '#', current: true },
+        { id: 3, text: 'MOVIES', url: '#', current: false },
+        { id: 4, text: 'TV', url: '#', current: false },
+        { id: 5, text: 'GAMES', url: '#', current: false },
+        { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+        { id: 7, text: 'VIDEOS', url: '#', current: false },
+        { id: 8, text: 'FANS', url: '#', current: false },
+        { id: 9, text: 'NEWS', url: '#', current: false },
+        { id: 10, text: 'SHOP', url: '#', current: false },
+
+    ]
+
     return (
         <nav>
             <ul>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+                {links.map(link => (
+                    <li key={link.id}>
+                        <a href={link.url} className={link.current ? "active" : ""}>
+                            {link.text}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
